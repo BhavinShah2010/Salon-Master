fbb6e08ab300e0ba98be98e749e0b9e8a80e642c
Sagar Kothari <sagar@Sagars-MacBook-Pro.local>
2015-10-23 12:13:21 +0530

    express validator installed

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var validator = require('express-validator');
 
 var session = require('express-session');
 var mongoose = require('mongoose');
@@ -25,6 +26,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(validator()); // to validate the input data.
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
