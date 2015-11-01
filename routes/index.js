1d0166cb4a0d98852596f0d54524a4e8651d0e0e
Altaf Shaikh <altafshaikh1717@gmail.com>
2015-11-01 15:19:34 +0530

    Demo Of header Footer

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	console.log();
-  res.render('login_master', { title: 'Sagar' });
+  res.render('login_master1', { title: 'Sagar' });
 });
+
 
 
 module.exports = router;
