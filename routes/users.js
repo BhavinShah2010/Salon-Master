bea267a8512a191cd2d4f601addffc98b8919d9b
happinezz <karansoni94@gmail.com>
2015-11-04 00:23:19 +0530

    after npm update and user model methods

--- routes/users.js
+++ routes/users.js
@@ -4,37 +4,7 @@ var user = require('../modules/user');
 var address = require('../modules/address');
 var passport = require('./../auth');
 
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('contactUs');
-});
-
-// To check username is available or not
-router.get('/checkUname', function(req,res){
-	user.findOne({username:req.query.username},function(err, users) {
-	  if(users){
-	  		res.send('Username not Available');
-	  	}
-	  	else{
-	  		res.send('Available');
-	  	}
-	});
-});
-
-
-router.post('/login',passport.authenticate('local',{
-	failureRedirect:'/',
-	successRedirect:'/users/'
-	
-}));
-
-router.get('/logout',function(req,res){
-	req.logout();
-	res.redirect('/');
-});
-
-
+//Register new User
 router.post('/add',function(req,res)
 {
 	//refer : https://github.com/chriso/validator.js#validators
@@ -87,5 +57,106 @@ router.post('/add',function(req,res)
 		})
   }
 });
+
+//Login
+router.post('/login',passport.authenticate('local',{
+	failureRedirect:'/',
+	successRedirect:'/salons/'
+	
+}));
+
+//Logout
+router.get('/logout',function(req,res){
+	req.logout();
+	res.redirect('/');
+});
+
+//View Profile
+router.post('/getDetails',function(req,res){
+    data=req.body;
+    var objectId=data.objectId;
+    user.find({ "_id": objectId }).exec(function(err, data) {
+  		if (err) throw err;
+  		res.send(data);
+		});
+
+});
+
+//Update Profile
+router.post('/updateProfile',function(req,res){
+	/*req.checkBody(  
+	  "phno", 
+	  "Enter a valid phone number.").isMobilePhone("en-US");
+
+	req.checkBody(  
+	  "email", 
+	   "Enter a valid Email").isEmail();
+
+	req.checkBody(  
+	  "gender",
+	  "Enter a valid Gender"
+	).isIn(['Male','Female','Other','male','female','other']);
+
+
+	req.checkBody(  
+	  "zipcode",
+	  "Enter a valid zipcode").isNumeric();
+
+	var errors = req.validationErrors();
+  	if (errors) {
+    	res.send(errors);
+    	return;
+    } else {
+  */  data=req.body;
+	var a=new address();
+	a.area=data.area;
+	a.city=data.city;
+	a.state=data.state;
+	user.findOneAndUpdate({ "_id": data.objectId }, { username: data.username, name:data.name, active:true, address:a}, function(err, updatedUser) {
+	a.zipcode=data.zipcode;
+  	if (err) throw err;
+  	user.find({ "_id": data.objectId}).exec(function(err, finaluser) {
+  	if(err) throw err;
+	console.log(finaluser);
+	})
+  })
+//  }
+});
+
+//active account
+
+//deactive Account
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('contactUs');
+});
+
+// To check username is available or not
+router.get('/checkUname', function(req,res){
+	user.findOne({username:req.query.username},function(err, users) {
+	  if(users){
+	  		res.send('Username not Available');
+	  	}
+	  	else{
+	  		res.send('Available');
+	  	}
+	});
+});
+
+
+router.post('/login',passport.authenticate('local',{
+	failureRedirect:'/',
+	successRedirect:'/users/'
+	
+}));
+
+router.get('/logout',function(req,res){
+	req.logout();
+	res.redirect('/');
+});
+
+
 
 module.exports = router;
