7087764d2c29ead5077f00789c3f7b1e3e556d38
happinezz <karansoni94@gmail.com>
2015-11-07 12:24:26 +0530

    small updations

--- modules/service.js
+++ modules/service.js
@@ -23,10 +23,4 @@ var schema = mongoose.Schema({
 	}
 });
 
-schema.methods.generateHash = function(password){
-	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
-schema.methods.validPassword = function(password){
-	return bcrypt.compareSync(password, this.password);
-};
 module.exports=mongoose.model('service',schema);
