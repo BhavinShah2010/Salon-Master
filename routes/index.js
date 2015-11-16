08c8a7be2c37b7bf9ae20cc8f49c9002f41c4e2a
Sagar Kothari <sagar@Sagars-MBP.connectify>
2015-11-16 16:45:31 +0530

    refined process

--- routes/index.js
+++ routes/index.js
@@ -7,17 +7,13 @@ var salon = require('../modules/salon');
 
 //redirect to login page if not logged in
 
-router.get('/', function(req, res, next) {
-	console.log();
-  res.render('login_master');
-});
 
 router.get('/contact', function(req, res, next) {
   console.log();
   res.render('contactUs');
 });
 
-router.get('/home', function(req, res, next) {
+router.get('/', function(req, res, next) {
     salon.find({}).populate('address').exec(function(err, salons) {
     if (err) throw err;
     console.log(salons.address)
@@ -36,11 +32,6 @@ router.get('/contactUs', function(req, res, next) {
   console.log();
   res.render('contactUs');
 });
-router.get('/user_profile', function(req, res, next) {
-  console.log();
-  //res.render('contactUs');
-   res.render('user_profile',{msg:req.message, views:req.session.views});
-});
 
 
 router.get('/map', function(req, res, next) {
@@ -51,15 +42,6 @@ router.get('/map', function(req, res, next) {
 router.get('/login', function(req, res, next) {
 	console.log();
   res.render('login_master');
-});
-router.get('/saloneP', function(req, res, next) {
-	console.log();
-  res.render('shop_profile1',{msg:req.message, views:req.session.views});
-});
-
-router.get('/shop_profile', function(req, res, next) {
-	console.log();
-  res.render('shop_profile');
 });
 
 
